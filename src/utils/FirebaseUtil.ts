7f8148bed7a3629b375e69d8aedd73c9b0fea789
yymin1022 <yymin1022@gmail.com>
2023-07-05 00:58:57 +0900

    Update FirebaseUtil : Apply ENV Variable for Firebase Keys

--- src/utils/FirebaseUtil.ts
+++ src/utils/FirebaseUtil.ts
@@ -1,3 +1,20 @@
+import {Firestore, getFirestore} from "@firebase/firestore";
+import {FirebaseApp, FirebaseOptions, initializeApp} from "@firebase/app";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+const firebaseConfig: FirebaseOptions = {
+    apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
+    appId: process.env.NEXT_PUBLIC_FB_APP_ID
+};
+
+let firebaseApp: FirebaseApp;
+let firebaseDB: Firestore;
+
 export const initFirebase = () => {
-    console.log("Init Firebase");
-}
+    firebaseApp =  initializeApp(firebaseConfig);
+    firebaseDB = getFirestore();
+};
