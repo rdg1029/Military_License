98442797535a524dea1af51acb62b1cc805e47dc
yymin1022 <yymin1022@gmail.com>
2023-07-05 02:38:10 +0900

    Update DataClass : Added Book Related Interfaces

--- src/utils/DataClass.ts
+++ src/utils/DataClass.ts
@@ -1,7 +1,7 @@
 export interface API_DATA{
     RESULT_CODE: number,
     RESULT_MSG: string,
-    RESULT_DATA: object
+    RESULT_DATA: LIST_DATA | object
 }
 
 export interface LIST_DATA{
@@ -9,20 +9,7 @@ export interface LIST_DATA{
 }
 
 export interface BOOK_DATA{
-    class_no_type: string,
-    ddc_class_no: string,
-    etc_class_no: string,
-    kdc_class_no: string,
-    lib_code: string,
-    lib_name: string,
-    marc_author: string,
-    marc_ea_isbn: string,
-    marc_page: string,
-    marc_price: string,
-    marc_publish_year: string,
-    marc_publisher: string,
-    marc_size: string,
-    marc_title: string,
-    rec_key: string,
-    status: string
+    author: string,
+    publisher: string,
+    title: string
 }
