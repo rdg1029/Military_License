dabb767b597bd6c5e250b93eb2d673846c56e333
rdg1029 <wcle3456@gmail.com>
2023-07-04 15:44:19 +0900

    Update ListTable.tsx : Apply key at elements in map()

--- src/pages/ListTable.tsx
+++ src/pages/ListTable.tsx
@@ -2,9 +2,9 @@
 <ListTable 
   tableHead={['랭킹', '계급', '이름', '획득MP', '취득 자격증 수']}
   tableData={[
-    [<p className="...">1</p>, <img src="..." />, "김푸앙", 120, 8],
-    [<p className="...">2</p>, <img src="..." />, "김대림", 110, 7],
-    [<p className="...">3</p>, <img src="..." />, "박안양", 100, 7],
+    [<p key={...} className="...">1</p>, <img key={...} src="..." />, "김푸앙", 120, 8],
+    [<p key={...} className="...">2</p>, <img key={...} src="..." />, "김대림", 110, 7],
+    [<p key={...} className="...">3</p>, <img key={...} src="..." />, "박안양", 100, 7],
     ...
   ]} />
 */
@@ -24,10 +24,10 @@ const ListTable = (props: Props) => {
   return (isMounted && 
   <table className="w-full text-center">
       <thead className="text-xs text-orange-600 border-b border-orange-600">
-        {props.tableHead.map((thData) => <th className="font-medium">{thData}</th>)}
+        {props.tableHead.map((thData, index) => <th key={index} className="font-medium">{thData}</th>)}
       </thead>
       <tbody>
-        {props.tableData.map((row) => <tr>{row.map((col) => <td className="border-b border-orange-600 pt-1.5 pb-1.5">{col}</td>)}</tr>)}
+        {props.tableData.map((row, index) => <tr key={index}>{row.map((col, index) => <td key={index} className="border-b border-orange-600 pt-1.5 pb-1.5">{col}</td>)}</tr>)}
       </tbody>
     </table>
   );
