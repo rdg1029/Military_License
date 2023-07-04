4c2df62ac70b473cb8dc64e72fb808c1e5b7d29d
yymin1022 <yymin1022@gmail.com>
2023-07-05 02:54:14 +0900

    Update getNaverBook : Applied Basic Setup of Naver API

--- src/pages/api/book/getNaverBook/[keyword].ts
+++ src/pages/api/book/getNaverBook/[keyword].ts
@@ -1,9 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {API_DATA} from "@/utils/DataClass";
 
-type Data = {
-  name: string
-}
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default function handler(
   req: NextApiRequest,
@@ -16,6 +16,12 @@ export default function handler(
     RESULT_MSG: "Ready",
     RESULT_DATA: {}
   }
+
+  const NAVER_API_URL = `https://openapi.naver.com/v1/search/book.xml?display=10&start=1&query=${keyword![0]}`
+  const NAVER_API_REQ = {
+    "X-Naver-Client-Id": process.env.NEXT_PUBLIC_NAVER_API_ID,
+    "X-Naver-Client-Secret": process.env.NEXT_PUBLIC_NAVER_API_SECRET
+  };
 
   res.send(RESULT_DATA);
 }
