5ebaac961c4e5e4238cb6e2a274a5fa89201ba58
yymin1022 <yymin1022@gmail.com>
2023-07-05 03:38:49 +0900

    Update getNaverBook : Applied API  Transaction

--- src/pages/api/book/getNaverBook/[keyword].ts
+++ src/pages/api/book/getNaverBook/[keyword].ts
@@ -1,11 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import {API_DATA} from "@/utils/DataClass";
+import {API_DATA, BOOK_DATA} from "@/utils/DataClass";
 
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<API_DATA>
 ) {
@@ -17,11 +18,33 @@ export default function handler(
     RESULT_DATA: {}
   }
 
-  const NAVER_API_URL = `https://openapi.naver.com/v1/search/book.xml?display=10&start=1&query=${keyword![0]}`
-  const NAVER_API_REQ = {
+  const NAVER_API_URL = `https://openapi.naver.com/v1/search/book.json?display=30&start=1&query=${keyword}`
+  const NAVER_API_HEADER = {
     "X-Naver-Client-Id": process.env.NEXT_PUBLIC_NAVER_API_ID,
     "X-Naver-Client-Secret": process.env.NEXT_PUBLIC_NAVER_API_SECRET
   };
+
+  await axios.get(NAVER_API_URL, {
+    headers: NAVER_API_HEADER
+  }).then((axiosRes)=>{
+      let listBook: Array<BOOK_DATA> = [];
+
+      axiosRes.data["items"].forEach((book: BOOK_DATA) => {
+          let tmpBook: BOOK_DATA = {
+            author: book["author"],
+            publisher: book["publisher"],
+            title: book["title"]
+          }
+          listBook.push(tmpBook);
+      });
+
+      RESULT_DATA.RESULT_CODE = 200;
+      RESULT_DATA.RESULT_MSG = "Success";
+      RESULT_DATA.RESULT_DATA = {data: listBook};
+  }).catch((error)=>{
+      RESULT_DATA.RESULT_CODE = 100;
+      RESULT_DATA.RESULT_MSG = error as string;
+  });
 
   res.send(RESULT_DATA);
 }
