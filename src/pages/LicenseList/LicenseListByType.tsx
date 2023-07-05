ab61f16ff082997d9d40ffcd9b8e92bfe17efe98
rdg1029 <wcle3456@gmail.com>
2023-07-05 16:47:14 +0900

    Update LicenseListByType.tsx : Apply key in elements

--- src/pages/LicenseList/LicenseListByType.tsx
+++ src/pages/LicenseList/LicenseListByType.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const LicenseListByType = () => {
   const dummyArray: React.ReactNode[][] = [];
   for (let i = 0; i < 25; i++) {
-    dummyArray.push([<p key={i} className="text-xs text-lime-800">굴삭기운전기능사</p>, <p className="text-xs text-lime-800">146. 건설기계 운전</p>, <p className="text-xs text-lime-800">한국산업인력공단</p>])
+    dummyArray.push([<p key={i} className="text-xs text-lime-800">굴삭기운전기능사</p>, <p key={i} className="text-xs text-lime-800">146. 건설기계 운전</p>, <p key={i} className="text-xs text-lime-800">한국산업인력공단</p>])
   }
 
   const [selectedType, setType] = useState("증분류 선택");
