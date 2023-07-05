b993b325d1f302302f1da050f2618f70975ee4d5
yymin1022 <yymin1022@gmail.com>
2023-07-06 03:24:46 +0900

    Update License API : Added Transaction for getDetail API Function

--- src/pages/api/license/getDetail/[code].ts
+++ src/pages/api/license/getDetail/[code].ts
@@ -1,13 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getLicenseDetail, initFirebase } from "@/utils/FirebaseUtil";
+import { API_DATA } from "@/utils/DataClass";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<API_DATA>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { code } = req.query;
+
+  initFirebase();
+  res.send(await getLicenseDetail(code!.toString()));
 }
