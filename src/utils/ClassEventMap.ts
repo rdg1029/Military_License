3064b142dc6002531ef3f8993fd6fee6f3b57c48
HarenKei <seungmin1266@gmail.com>
2023-07-06 18:13:55 +0900

    Add ClassEventMap.ts

---
+++ src/utils/ClassEventMap.ts
@@ -0,0 +1,15 @@
+const ArmyClassEventMap = [
+  {
+    classCode: 1,
+    className: "보병",
+    eventArr: []
+  },
+  {
+    classCode: 2,
+    clssName: "기갑",
+    eventArr: []
+  },
+  {
+    classCode: 3,
+  }
+]
