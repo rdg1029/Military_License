8a684d9701a867a23756eb8592abe60ad87a338b
yymin1022 <yymin1022@gmail.com>
2023-07-06 17:00:02 +0900

    Update DataClass : Added License List Related Interface

--- src/utils/DataClass.ts
+++ src/utils/DataClass.ts
@@ -5,7 +5,7 @@ export interface API_DATA{
 }
 
 export interface LIST_DATA{
-    data: Array<BOOK_DATA>
+    data: Array<BOOK_DATA | LICENSE_LIST_DATA>
 }
 
 export interface BOOK_DATA{
@@ -17,6 +17,18 @@ export interface BOOK_DATA{
 export interface LICENSE_DATA{
     content: string,
     schedule: Array<string>,
+    strGualgbcd: string,
+    strGualgbnm: string,
+    strJmfldnm: string,
+    strMdobligfldcd: string,
+    strMdobligfldnm: string,
+    strObligfldcd: string,
+    strObligfldnm: string,
+    strSeriescd: string,
+    strSeriesnm: string
+}
+
+export interface LICENSE_LIST_DATA{
     strGualgbcd: string,
     strGualgbnm: string,
     strJmfldnm: string,
