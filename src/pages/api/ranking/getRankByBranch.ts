6dbc669ce5357fe34fdbf253c339766b1bb8d99a
yymin1022 <yymin1022@gmail.com>
2023-07-07 02:29:32 +0900

    Update Rank API : Added Transaction of getRankByBranch

--- src/pages/api/ranking/getRankByBranch.ts
+++ src/pages/api/ranking/getRankByBranch.ts
@@ -1,13 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getRankByBranch, initFirebase } from "@/utils/FirebaseUtil";
+import { API_DATA } from "@/utils/DataClass";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<API_DATA>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  initFirebase();
+  let data = await getRankByBranch();
+  res.send(data);
 }
