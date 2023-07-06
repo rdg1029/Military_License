18c115ba448e56f256b61c315973ae2bea3257fb
HarenKei <seungmin1266@gmail.com>
2023-07-07 01:15:20 +0900

    Update ClassEventMap.ts : Add Army Class Data

--- src/utils/ClassEventMap.ts
+++ src/utils/ClassEventMap.ts
@@ -2,14 +2,101 @@ const ArmyClassEventMap = [
   {
     classCode: 1,
     className: "보병",
-    eventArr: []
+    eventArr: [2, 12, 14, 16, 20, 25, 26]
   },
   {
     classCode: 2,
     clssName: "기갑",
-    eventArr: []
+    eventArr: [15, 16, 17, 18, 20, 21, 25, 26]
   },
   {
     classCode: 3,
-  }
+    clssName: "포병",
+    eventArr: [15, 16, 17, 18, 20, 21, 25, 26]
+  },
+  {
+    classCode: 4,
+    clssName: "방공",
+    eventArr: [15, 16, 17, 18, 20, 21, 25, 26]
+  },
+  {
+    classCode: 5,
+    clssName: "항공",
+    eventArr: [15, 16, 17, 18, 20, 21, 25, 26]
+  },
+  {
+    classCode: 6,
+    clssName: "정보",
+    eventArr: [2, 20, 21]
+  },
+  {
+    classCode: 7,
+    clssName: "정보통신",
+    eventArr: [2, 20, 21]
+  },
+  {
+    classCode: 8,
+    clssName: "화생방",
+    eventArr: [15, 17, 18, 20, 25, 26]
+  },
+  {
+    classCode: 9,
+    clssName: "공병",
+    eventArr: [14, 15, 16, 17, 20, 21, 25]
+  },
+  {
+    classCode: 10,
+    clssName: "군수",
+    eventArr: [2, 15, 16, 17, 18, 20, 21, 25, 26]
+  },
+  {
+    classCode: 11,
+    clssName: "병기",
+    eventArr: [2, 15, 16, 17, 18, 20, 21, 25, 26]
+  },
+  {
+    classCode: 12,
+    clssName: "병참",
+    eventArr: [2, 15, 16, 17, 18, 20, 21, 25, 26]
+  },
+  {
+    classCode: 13,
+    clssName: "수송",
+    eventArr: [2, 9, 14, 16, 17, 20, 25, 26]
+  },
+  {
+    classCode: 14,
+    clssName: "인사",
+    eventArr: [2, 7]
+  },
+  {
+    classCode: 15,
+    clssName: "재정",
+    eventArr: [2, 7]
+  },
+  {
+    classCode: 16,
+    clssName: "군사경찰",
+    eventArr: [2, 7]
+  },
+  {
+    classCode: 17,
+    clssName: "공보정훈 / 군악",
+    eventArr: [2, 8, 12, 21, 23]
+  },
+  {
+    classCode: 18,
+    clssName: "의무",
+    eventArr: [2, 6, 26]
+  },
+  {
+    classCode: 19,
+    clssName: "법무",
+    eventArr: [2]
+  },
+  {
+    classCode: 20,
+    clssName: "군종",
+    eventArr: [2, 7]
+  },
 ]
