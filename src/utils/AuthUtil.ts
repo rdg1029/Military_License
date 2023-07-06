eea8864d950d9b6377da89644e619067faf7c8be
yymin1022 <yymin1022@gmail.com>
2023-07-07 05:15:11 +0900

    Update Auth Util : Added from Template

---
+++ src/utils/AuthUtil.ts
@@ -0,0 +1,23 @@
+import { credential } from "firebase-admin";
+import { initializeApp } from 'firebase-admin/app';
+import { getAuth } from "firebase-admin/auth";
+
+export const initFirebaseAuth = () => {
+    let serviceAccount = require("firebase-admin-account.json");
+    // let serviceAccount = require("D:\\Projects\\Web\\ECOmunity-BE\\firebase-admin-account.json");
+    initializeApp({
+        credential: credential.cert(serviceAccount)
+    });
+}
+
+export const verifyToken = (strToken: string) => {
+    getAuth()
+        .verifyIdToken(strToken)
+        .then((decodedToken) => {
+            return decodedToken.uid;
+        })
+        .catch((error) => {
+            console.log(error);
+            return -1;
+        });
+}
