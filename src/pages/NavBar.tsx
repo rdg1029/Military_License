1a0ac0dd01b895392e755f14f8261b68adc89483
HarenKei <seungmin1266@gmail.com>
2023-07-08 02:32:36 +0900

    Update NavBar.tsx : Edit Link Positions

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -1,52 +1,62 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faIdCard, faRankingStar, faRightToBracket, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHouse,
+  faIdCard,
+  faRankingStar,
+  faRightToBracket,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   const [isLogin, setIsLogin] = useState(false);
 
   return (
     <div className="flex flex-row justify-evenly items-center w-full h-16 bg-df-orange-opacity fixed bottom-0">
-      <div className="flex flex-col justify-center items-center cursor-pointer">
-        <FontAwesomeIcon icon={faHouse} size="1x" color="#E86A33" />
-        <Link href="/Main">
+      <Link href="/Main">
+        <div className="flex flex-col justify-center items-center cursor-pointer">
+          <FontAwesomeIcon icon={faHouse} size="1x" color="#E86A33" />
           <p className="text-df-orange font-bold">메인</p>
-        </Link>
-      </div>
+        </div>
+      </Link>
 
-      <div className="flex flex-col justify-center items-center cursor-pointer">
-        <FontAwesomeIcon icon={faIdCard} size="1x" color="#E86A33" />
-        <Link href="/LicenseList">
+      <Link href="/LicenseList">
+        <div className="flex flex-col justify-center items-center cursor-pointer">
+          <FontAwesomeIcon icon={faIdCard} size="1x" color="#E86A33" />
           <p className="text-df-orange font-bold">자격증</p>
-        </Link>
-      </div>
+        </div>
+      </Link>
 
-      <div className="flex flex-col justify-center items-center cursor-pointer">
-        <FontAwesomeIcon icon={faRankingStar} size="1x" color="#E86A33" />
-        <Link href="/RankList">
+      <Link href="/RankList">
+        <div className="flex flex-col justify-center items-center cursor-pointer">
+          <FontAwesomeIcon icon={faRankingStar} size="1x" color="#E86A33" />
           <p className="text-df-orange font-bold">랭킹</p>
-        </Link>
-      </div>
+        </div>
+      </Link>
 
-      {isLogin ?
-        <div className="flex flex-col justify-center items-center cursor-pointer">
-          <FontAwesomeIcon icon={faUser} size="1x" color="#E86A33"/>
-          <Link href="/UserProfile">
+      {isLogin ? (
+        <Link href="/UserProfile">
+          <div className="flex flex-col justify-center items-center cursor-pointer">
+            <FontAwesomeIcon icon={faUser} size="1x" color="#E86A33" />
+
             <p className="text-df-orange font-bold">프로필</p>
-          </Link>
-        </div>
-        :
-        <div className="flex flex-col justify-center items-center cursor-pointer">
-          <FontAwesomeIcon icon={faRightToBracket} size="1x" color="#E86A33"/>
-          <Link href="/Login">
+          </div>
+        </Link>
+      ) : (
+        <Link href="/Login">
+          <div className="flex flex-col justify-center items-center cursor-pointer">
+            <FontAwesomeIcon
+              icon={faRightToBracket}
+              size="1x"
+              color="#E86A33"
+            />
             <p className="text-df-orange font-bold">로그인</p>
-          </Link>
-        </div>
-      }
+          </div>
+        </Link>
+      )}
     </div>
   );
 };
 
 export default NavBar;
-
