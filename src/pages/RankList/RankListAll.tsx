0ab496c4ef7f71a4a901bc136b5ddd0043cee9d2
HarenKei <seungmin1266@gmail.com>
2023-07-08 01:54:35 +0900

    Update RankListAll.tsx : Edit Dummy Data

--- src/pages/RankList/RankListAll.tsx
+++ src/pages/RankList/RankListAll.tsx
@@ -2,12 +2,12 @@ import ListTable from "../ListTable";
 
 const RankListAll = () => {
   const dummyArray: React.ReactNode[][] = [];
-  dummyArray.push([<p key={1} className="text-2xl text-yellow-500">1</p>, <p key={1} className="text-2xl text-df-green">병장</p>, <p key={1} className="text-2xl text-df-green">정승민</p>, <p key={1} className="text-2xl text-df-green">200</p>]);
-  dummyArray.push([<p key={2} className="text-2xl text-blue-900">2</p>, <p key={2} className="text-2xl text-df-green">병장</p>, <p key={2} className="text-2xl text-df-green">정승민</p>, <p key={2} className="text-2xl text-df-green">180</p>]);
-  dummyArray.push([<p key={3} className="text-2xl text-orange-900">3</p>, <p key={3} className="text-2xl text-df-green">병장</p>, <p key={3} className="text-2xl text-df-green">정승민</p>, <p key={3} className="text-2xl text-df-green">170</p>]);
+  dummyArray.push([<p key={1} className="text-2xl text-yellow-500">1</p>, <p key={1} className="text-2xl text-df-green">병장</p>, <p key={1} className="text-2xl text-df-green">정승민</p>, <p key={1} className="text-2xl text-df-green">200</p>, <p key={1} className="text-2xl text-df-green">14</p>]);
+  dummyArray.push([<p key={2} className="text-2xl text-blue-900">2</p>, <p key={2} className="text-2xl text-df-green">병장</p>, <p key={2} className="text-2xl text-df-green">정승민</p>, <p key={2} className="text-2xl text-df-green">180</p>, <p key={2} className="text-2xl text-df-green">13</p>]);
+  dummyArray.push([<p key={3} className="text-2xl text-orange-900">3</p>, <p key={3} className="text-2xl text-df-green">병장</p>, <p key={3} className="text-2xl text-df-green">정승민</p>, <p key={3} className="text-2xl text-df-green">170</p>, <p key={3} className="text-2xl text-df-green">12</p>]);
   
-  for (let i = 4; i < 10; i++) {
-    dummyArray.push([<p key={i} className="text-2xl text-blue-900">{i}</p>, <p key={i} className="text-2xl text-df-green">병장</p>, <p key={i} className="text-2xl text-df-green">정승민</p>, <p key={i} className="text-2xl text-df-green">{170 - (i * 2)}</p>]);
+  for (let i = 4; i <= 10; i++) {
+    dummyArray.push([<p key={i} className="text-2xl text-df-green">{i}</p>, <p key={i} className="text-2xl text-df-green">병장</p>, <p key={i} className="text-2xl text-df-green">정승민</p>, <p key={i} className="text-2xl text-df-green">{170 - (i * 2)}</p>, <p key={i} className="text-2xl text-df-green">{11 - (i - 4)}</p>]);
   }
 
   return (
