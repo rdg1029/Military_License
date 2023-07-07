470a722c480dac931bfb224ecbbff1206187d96b
HarenKei <seungmin1266@gmail.com>
2023-07-08 01:51:28 +0900

    Update RankList.tsx : Import AllList

--- src/pages/RankList/RankList.tsx
+++ src/pages/RankList/RankList.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import ListPage from "../ListPage";
+import ListAll from "./RankListAll";
 
 const RankList = () => {
   return(
-    <></>
+    <ListPage
+      title="밀리센스 랭킹"
+      tabs={[
+        {name: "우리 부대 랭킹", component: <ListAll />},
+        // {name: "병과별 추천", component: <ListByClass />},
+        // {name: "분야별 추천", component: <ListByType />},
+        // {name: "전체 취득순", component: <ListInOrder />},
+      ]} />
   );
 };
 
