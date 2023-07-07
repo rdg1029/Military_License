087a984532c9d8791bcb47a8bfbd4ddfef84a09c
HarenKei <seungmin1266@gmail.com>
2023-07-08 02:01:59 +0900

    Update RankList.tsx : Import RankListANA

--- src/pages/RankList/RankList.tsx
+++ src/pages/RankList/RankList.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ListPage from "../ListPage";
 import ListAll from "./RankListAll";
+import ListANA from "./RankListANA";
 
 const RankList = () => {
   return(
     <ListPage
       title="밀리센스 랭킹"
       tabs={[
-        {name: "우리 부대 랭킹", component: <ListAll />},
+        {name: "군종별 랭킹", component: <ListANA/>},
+        {name: "우리 부대 랭킹", component: <ListAll/>},
         // {name: "병과별 추천", component: <ListByClass />},
         // {name: "분야별 추천", component: <ListByType />},
         // {name: "전체 취득순", component: <ListInOrder />},
