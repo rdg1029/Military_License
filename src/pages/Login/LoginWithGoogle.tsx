14254e664c6ef4d65707689b06cc52b1d04a8f0c
HarenKei <seungmin1266@gmail.com>
2023-07-08 17:47:26 +0900

    Updte Login.tsx, LoginWithGoogle.tsx : Add Styles

--- src/pages/Login/LoginWithGoogle.tsx
+++ src/pages/Login/LoginWithGoogle.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { SignInGoogle, initFirebase } from "@/utils/FirebaseUtil";
+
+const clickHandler = () => {
+  initFirebase();
+  SignInGoogle();
+};
 
 const LoginWithGoogle = () => {
-  return(
-    <>
-    </>
+  return (
+    <div>
+
+      <button
+        className="w-72 h-20 bg-df-orange-opacity text-df-green rounded-2xl drop-shadow-md"
+        onClick={clickHandler}
+      >
+        <div className="w-full h-full flex flex-row justify-evenly items-center">
+          <FontAwesomeIcon icon={faGoogle} size="2x" color="#E86A33"/>
+          <p className="font-extrabold text-2xl text-df-orange">구글 계정으로 로그인</p>
+        </div>
+      </button>
+    </div>
   );
 };
 
