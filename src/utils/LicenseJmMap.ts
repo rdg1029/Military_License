7a655ca368a420b50677eff95f69eeeeee69af82
HarenKei <seungmin1266@gmail.com>
2023-07-09 02:59:26 +0900

    Add LicenseJmMap.ts

---
+++ src/utils/LicenseJmMap.ts
@@ -0,0 +1,90 @@
+export const JmMap = [
+  {
+    JmCode: "02",
+    JmName: "경영/회계/사무"
+  },
+  {
+    JmCode: "04",
+    JmName: "교육/자연과학/사회과학"
+  },
+  {
+    JmCode: "06",
+    JmName: "보건/의료"
+  },
+  {
+    JmCode: "07",
+    JmName: "사회복지/종교"
+  },
+  {
+    JmCode: "08",
+    JmName: "문화/예술/디자인/방송"
+  },
+  {
+    JmCode: "09",
+    JmName: "운전/운송"
+  },
+  {
+    JmCode: "10",
+    JmName: "영업/판매"
+  },
+  {
+    JmCode: "12",
+    JmName: "이용/숙박/여행/오락/스포츠"
+  },
+  {
+    JmCode: "13",
+    JmName: "음식서비스"
+  },
+  {
+    JmCode: "14",
+    JmName: "건설"
+  },
+  {
+    JmCode: "15",
+    JmName: "광업자원"
+  },
+  {
+    JmCode: "16",
+    JmName: "기계"
+  },
+  {
+    JmCode: "17",
+    JmName: "재료"
+  },
+  {
+    JmCode: "18",
+    JmName: "화학"
+  },
+  {
+    JmCode: "19",
+    JmName: "섬유/의복"
+  },
+  {
+    JmCode: "20",
+    JmName: "전기/전자"
+  },
+  {
+    JmCode: "21",
+    JmName: "정보/통신"
+  },
+  {
+    JmCode: "22",
+    JmName: "식품가공"
+  },
+  {
+    JmCode: "23",
+    JmName: "인쇄/목재/가구/공예"
+  },
+  {
+    JmCode: "24",
+    JmName: "농림/어업"
+  },
+  {
+    JmCode: "25",
+    JmName: "안전관리"
+  },
+  {
+    JmCode: "26",
+    JmName: "환경/에너지"
+  }
+];
