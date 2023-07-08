160cce7f25e844aab338f174ab406f8ed021490a
HarenKei <seungmin1266@gmail.com>
2023-07-09 02:46:06 +0900

    Update RankListUnit.tsx : API integration completed

--- src/pages/RankList/RankListUnit.tsx
+++ src/pages/RankList/RankListUnit.tsx
@@ -1,20 +1,79 @@
+import { useState, useEffect } from "react";
+import { RANK_UNIT_DATA } from "@/utils/DataClass";
 import ListTable from "../ListTable";
 
-const RankListUnit = () => {
-  const dummyArray: React.ReactNode[][] = [];
-  dummyArray.push([<p key={1} className="text-2xl text-yellow-500">1</p>, <p key={1} className="text-xl text-df-green">제8기동사단</p>, <p key={1} className="text-2xl text-df-green">1200</p>, <p key={1} className="text-2xl text-df-green">200</p>]);
-  dummyArray.push([<p key={2} className="text-2xl text-blue-900">2</p>, <p key={2} className="text-xl text-df-green">방공관제사령부</p>, <p key={2} className="text-2xl text-df-green">1000</p>, <p key={2} className="text-2xl text-df-green">180</p>]);
-  dummyArray.push([<p key={3} className="text-2xl text-orange-900">3</p>, <p key={3} className="text-xl text-df-green">제22보병사단</p>, <p key={3} className="text-2xl text-df-green">800</p>, <p key={3} className="text-2xl text-df-green">160</p>]);
-  dummyArray.push([<p key={4} className="text-2xl text-df-green">4</p>, <p key={4} className="text-xl text-df-green">제11기동사단</p>, <p key={4} className="text-2xl text-df-green">600</p>, <p key={4} className="text-2xl text-df-green">140</p>]);
-  dummyArray.push([<p key={5} className="text-2xl text-df-green">5</p>, <p key={5} className="text-xl text-df-green">육군사관학교</p>, <p key={5} className="text-2xl text-df-green">400</p>, <p key={5} className="text-2xl text-df-green">120</p>]);
-  
-  return (
-      <div className="grow flex flex-col w-full overflow-y-scroll">
-        <ListTable
-          tableHead={["랭킹", "부대명", "획득 MP", "취득 자격증 수"]}
-          tableData={dummyArray} />
-      </div>
-  );
+interface Props {
+  list: Array<RANK_UNIT_DATA>;
 }
+
+const RankListUnit = ({ list }: Props) => {
+  const [rankData, setRankData] = useState<React.ReactNode[][]>([]);
+
+  useEffect(() => {
+    let tmpArray: React.ReactNode[][] = [];
+    for (let i = 0; i < list.length; i++) {
+      if (i + 1 === 1) {
+        tmpArray.push([
+          <p key={i} className="text-2xl text-df-gold">
+            {i + 1}
+          </p>,
+          <p key={i} className="text-xl text-df-green">
+            {list[i].name}
+          </p>,
+          <p key={i} className="text-2xl text-df-green">
+            {list[i].mp}
+          </p>,
+        ]);
+      } else if (i + 1 === 2) {
+        tmpArray.push([
+          <p key={i} className="text-2xl text-df-silver">
+            {i + 1}
+          </p>,
+          <p key={i} className="text-xl text-df-green">
+            {list[i].name}
+          </p>,
+          <p key={i} className="text-2xl text-df-green">
+            {list[i].mp}
+          </p>,
+        ]);
+      } else if (i + 1 === 3) {
+        tmpArray.push([
+          <p key={i} className="text-2xl text-df-bronze">
+            {i + 1}
+          </p>,
+          <p key={i} className="text-xl text-df-green">
+            {list[i].name}
+          </p>,
+          <p key={i} className="text-2xl text-df-green">
+            {list[i].mp}
+          </p>,
+        ]);
+      } else {
+        tmpArray.push([
+          <p key={i} className="text-2xl text-df-green">
+            {i + 1}
+          </p>,
+          <p key={i} className="text-xl text-df-green">
+            {list[i].name}
+          </p>,
+          <p key={i} className="text-2xl text-df-green">
+            {list[i].mp}
+          </p>,
+        ]);
+      }
+    }
+
+    setRankData(tmpArray);
+  }, [list]);
+
+  return (
+    <div className="grow flex flex-col w-full overflow-y-scroll">
+      <ListTable
+        tableHead={["랭킹", "부대명", "획득 MP"]}
+        tableData={rankData}
+      />
+    </div>
+  );
+};
 
 export default RankListUnit;
