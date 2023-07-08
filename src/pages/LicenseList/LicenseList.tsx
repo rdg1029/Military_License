b0caf1cb04f49c8c54f043c0157f8cc0038393e1
rdg1029 <wcle3456@gmail.com>
2023-07-08 17:24:56 +0900

    Update LicenseList.tsx : Apply data from getListAll

--- src/pages/LicenseList/LicenseList.tsx
+++ src/pages/LicenseList/LicenseList.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import ListPage from "../ListPage";
 import ListAll from "./LicenseListAll";
 import ListByClass from "./LicenseListByClass";
 import ListByType from "./LicenseListByType";
 import ListInOrder from "./LicenseListInOrder";
+import axios from "axios";
+import { LICENSE_LIST_DATA } from "@/utils/DataClass";
 
 const LicenseList = () => {
+  const [isLoaded, setLoaded] = useState<Boolean>(false);
+  const [listAll, setListAll] = useState<Array<LICENSE_LIST_DATA>>([]);
+
+  useEffect(() => {
+    const req = axios.create();
+    req.get("/api/license/getListAll")
+    .then(res => {
+      setListAll(res.data.RESULT_DATA.data);
+      setLoaded(true);
+      console.log(res);
+      console.log("all items loaded");
+    })
+    .catch(err => console.log(err));
+  }, []);
+
   return (
     <ListPage
       title="국가 기술 자격증 추천"
