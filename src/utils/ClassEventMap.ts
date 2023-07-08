0af969ba1ba9348b54c63425b7b363d7ca4b074d
HarenKei <seungmin1266@gmail.com>
2023-07-08 23:59:04 +0900

    Update ClassEventMap : Fix Typing Miss

--- src/utils/ClassEventMap.ts
+++ src/utils/ClassEventMap.ts
@@ -16,127 +16,127 @@ export const ClassEventMap = [
   },
   {
     classCode: "a4",
-    clssName: "방공",
+    className: "방공",
     eventArr: ["15", "16", "17", "18", "20", "21", "25", "26"]
   },
   {
     classCode: "a5",
-    clssName: "항공",
+    className: "항공",
     eventArr: ["15", "16", "17", "18", "20", "21", "25", "26"]
   },
   {
     classCode: "a6",
-    clssName: "정보",
+    className: "정보",
     eventArr: ["2", "20", "21"]
   },
   {
     classCode: "a7",
-    clssName: "정보통신",
+    className: "정보통신",
     eventArr: ["02", "20", "21"]
   },
   {
     classCode: "a8",
-    clssName: "화생방",
+    className: "화생방",
     eventArr: ["15", "17", "18", "20", "25", "26"]
   },
   {
     classCode: "a9",
-    clssName: "공병",
+    className: "공병",
     eventArr: ["14", "15", "16", "17","20", "21", "25"]
   },
   {
     classCode: "a10",
-    clssName: "군수",
+    className: "군수",
     eventArr: ["02", "15", "16", "17", "18", "20", "21", "25", "26"]
   },
   {
     classCode: "a11",
-    clssName: "병기",
+    className: "병기",
     eventArr: ["02", "15", "16", "17", "18", "20", "21", "25", "26"]
   },
   {
     classCode: "a12",
-    clssName: "병참",
+    className: "병참",
     eventArr: ["02", "15", "16", "17", "18", "20", "21", "25", "26"]
   },
   {
     classCode: "a13",
-    clssName: "수송",
+    className: "수송",
     eventArr: ["02", "09", "14", "16", "17", "20", "25", "26"]
   },
   {
     classCode: "a14",
-    clssName: "인사",
+    className: "인사",
     eventArr: ["02", "07"]
   },
   {
     classCode: "a15",
-    clssName: "재정",
+    className: "재정",
     eventArr: ["02", "07"]
   },
   {
     classCode: "a16",
-    clssName: "군사경찰",
+    className: "군사경찰",
     eventArr: ["02", "07"]
   },
   {
     classCode: "a17",
-    clssName: "공보정훈 / 군악",
+    className: "공보정훈 / 군악",
     eventArr: ["02", "08", "12", "21", "23"]
   },
   {
     classCode: "a18",
-    clssName: "의무",
+    className: "의무",
     eventArr: ["02", "06", "26"]
   },
   {
     classCode: "a19",
-    clssName: "법무",
+    className: "법무",
     eventArr: ["02"]
   },
   {
     classCode: "a20",
-    clssName: "군종",
+    className: "군종",
     eventArr: ["02", "07"]
   },
   {
     classCode: "a21",
-    clssName: "함정",
+    className: "함정",
     eventArr: ["15", "16", "17", "18", "20", "21", "25", "26"]
   },
   {
     classCode: "a22",
-    clssName: "보급",
+    className: "보급",
     eventArr: ["02", "15", "16", "17", "18", "20", "21", "25", "26"]
   },
   {
     classCode: "a23",
-    clssName: "조함",
+    className: "조함",
     eventArr: ["15", "16", "17", "18", "20", "21", "25", "26"]
   },
   {
     classCode: "a24",
-    clssName: "조종",
+    className: "조종",
     eventArr: ["15", "16", "17", "18", "20", "21", "25", "26"]
   },
   {
     classCode: "a25",
-    clssName: "항공통제",
+    className: "항공통제",
     eventArr: ["15", "16", "17", "18", "20", "21", "25", "26"]
   },
   {
     classCode: "a26",
-    clssName: "방공포병",
+    className: "방공포병",
     eventArr: ["15", "16", "17", "18", "20", "21", "25", "26"]
   },
   {
     classCode: "a27",
-    clssName: "기상",
+    className: "기상",
     eventArr: ["25", "26"]
   },
   {
     classCode: "a28",
-    clssName: "인사교육",
+    className: "인사교육",
     eventArr: ["02", "07"]
   },
 ];
